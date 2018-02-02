fa0e34761fcc6e3d5858e655c2cd27c9fbd9065c
cvrac <cvracdi@gmail.com>
2018-02-02 22:21:24 +0200

    Data distribution with parallel I/O done. Test with a simple write

--- convolute.c
+++ convolute.c
@@ -9,6 +9,16 @@
 #define MASTER_PROCESS 0
 #define DIMENSIONALITY 2
 
+
+/*
+ * 1. Find process row and column offsets and vector definition for the input data
+ * 2. Convolution filter definition
+ * 3. Parallel read of the input file "image"
+ *
+*/
+
+
+
 unsigned int best_fit(int rows, int cols, int processes) {
 
 	unsigned int i, rows_it, cols_it, best_fit_val = 0, min = rows + cols + 1;
@@ -40,6 +50,7 @@ int main(int argc, char **argv) {
 	char *picture = NULL;
     int rows = 0, columns = 0;
     int proc_row = -1, proc_col = -1;
+    int i = 0;
 
 	MPI_Init(&argc, &argv);
 
@@ -55,6 +66,7 @@ int main(int argc, char **argv) {
 	MPI_Request rcv_ea_request;
 	MPI_Request rcv_so_request;	
 	MPI_Request rcv_we_request;	
+    MPI_Status status;
 	
 	MPI_Datatype send_col, send_row;
 
@@ -92,7 +104,7 @@ int main(int argc, char **argv) {
 	}
 
 
-	picture = malloc((strlen(argv[5]) +1) * sizeof(char));
+	picture = calloc((strlen(argv[5]) +1), sizeof(char));
 	assert( picture != NULL);
 		
 	strncpy(picture, argv[5], strlen(argv[5]) + 1);
@@ -106,12 +118,77 @@ int main(int argc, char **argv) {
     MPI_Bcast(&columns, 1, MPI_INT, MASTER_PROCESS, cartesianComm);
     MPI_Bcast(&best_fit_rows, 1, MPI_INT, MASTER_PROCESS, cartesianComm);
 
-    // Find process row and column offsets
+    //1. Find process row and column offsets and vector definition for the input data
     
     int row_index = (comm_rk / (comm_sz / best_fit_rows))* rows;
     int column_index = (comm_rk % (comm_sz / best_fit_rows)) * columns;
 
+    //swapping between source and destination vectors, using a temp vector
+    char *source_vec = NULL, *destination_vec = NULL, *temp_vec = NULL;
+
+    //multiplier shall be 3 for RGB input pictures, 1 for GREY input pictures
+    unsigned int multiplier = (grey == 1) ? 1 : 3;
+
+    int total_sz = width * height * multiplier;
+
+    source_vec = calloc((rows + 2) * (columns + 2) * multiplier, sizeof(char));
+    destination_vec = calloc((rows + 2) * (columns + 2) * multiplier, sizeof(char));
+
+    assert(source_vec != NULL && destination_vec != NULL);
+
 	printf("I am the process %d %d, %d, %d, %d, %s with starting row index = %d and starting column index = %d\n", comm_rk, width, height, loops, grey, picture, row_index, column_index);
+    
+    //2. Convolution filter definition
+    float filter[3][3] = {{1/16.0, 2/16.0, 1/16.0},
+                          {2/16.0, 4/16.0, 2/16.0},
+                          {1/16.0, 2/16.0, 1/16.0}};
+
+
+    //3. Parallel read of the input file "image"
+    
+    MPI_File picture_file = NULL;
+    MPI_File_open(cartesianComm, picture, MPI_MODE_RDONLY, MPI_INFO_NULL, &picture_file);
+
+    /*
+     * Each process reads the corresponding block of data from the file, and stores it
+     * to the appropriate index
+     */
+
+    for (i = 0; i < rows; i++) {
+        MPI_File_seek(picture_file, multiplier * ((row_index + i) * width + column_index), MPI_SEEK_SET);
+        MPI_File_read(picture_file, source_vec + multiplier * columns * i + multiplier, multiplier * columns, MPI_BYTE, &status);
+    }
+
+    MPI_File_close(&picture_file);
+
+    printf("read file\n");
+
+
+    char *outputImage = calloc(strlen("out_image.raw") + 1, sizeof(char));
+    strncpy(outputImage, "out_image.raw", strlen("out_image.raw"));
+
+    printf("%s\n", outputImage);
+    MPI_File picture_file_out = NULL;
+    MPI_File_open(cartesianComm, outputImage, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &picture_file_out);
+
+    /*
+     * Each process reads the corresponding block of data from the file, and stores it
+     * to the appropriate index
+     */
+
+    for (i = 0; i < rows; i++) {
+        MPI_File_seek(picture_file_out, multiplier * ((row_index + i) * width + column_index), MPI_SEEK_SET);
+        MPI_File_write(picture_file_out, source_vec + multiplier * columns * i + multiplier, multiplier * columns, MPI_BYTE, &status);
+    }
+
+    MPI_File_close(&picture_file_out);
+
+   
+
+    free(source_vec);
+    free(destination_vec);
+    free(outputImage);
+    free(picture);
 
 	MPI_Finalize();
 	return 0;
